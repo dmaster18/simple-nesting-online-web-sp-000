45f9897ea2e6d4dc29e1c711a54dddfbe9b110e8
David Strauch <dmaster18@gmail.com>
2019-08-05 15:34:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,7 +133,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages][1] = 
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
 end
 
 
