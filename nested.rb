7ebfad058e5c467e392f0222aed15149f9b3eff0
David Strauch <dmaster18@gmail.com>
2019-08-05 15:23:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
   programmer_hash[:yukihiro_matsumoto] = {}
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
   programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-  return programmer_hash  
+  return programmer_hash[:yukihiro_matsumoto]  
 end
 
 puts adding_matz
